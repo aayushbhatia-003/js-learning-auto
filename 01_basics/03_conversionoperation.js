076e4490ba8d036044ed890e4c596e50be65d8b1
aayushbhatia-003 <bhatiaaayush24@gmail.com>
2025-03-30 09:15:04 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,9 @@
+let score="33"
+
+
+let valueInNumber = Number(score)
+let isLoggedIn ="aayush"
+
+console.log(typeof valueInNumber);
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
