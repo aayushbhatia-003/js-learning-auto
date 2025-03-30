03aefe2a1eacbffd83f0313e99f6f2f26eb1a537
aayushbhatia-003 <bhatiaaayush24@gmail.com>
2025-03-30 12:27:34 +0000

    comparisons

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -7,3 +7,15 @@ let isLoggedIn ="aayush"
 console.log(typeof valueInNumber);
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+
+
+//*********************** operations ************************
+
+let value = 3 
+let negvalue =-value
+
+let str1 = "hello"
+let str2 ="aayush"
+let str3 = str1+str2
+console.log(str3)
+
